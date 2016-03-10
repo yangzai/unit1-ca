db9812c10cc4752c9c79dabd4a7d71e29024c209
chenyao <E0015290@u.nus.edu>
2016-03-10 15:20:02 +0800

    Add constructor, getters and setters

--- src/sg/edu/nus/iss/se24_2ft/unit1/ca/Product.java
+++ src/sg/edu/nus/iss/se24_2ft/unit1/ca/Product.java
@@ -1,18 +1,88 @@
 package sg.edu.nus.iss.se24_2ft.unit1.ca;
 
-import java.math.BigDecimal;
-
 /**
  * Created by yangzai on 26/2/16.
  */
 public class Product {
     private String id, name, description;
     private int quantity, barCode, treshold, orderQuantity;
-//    BigDecimal price;
+    // BigDecimal price;
     private double price;
 
-    public Product() {
+    public Product(String id, String name, String description, int quantity, int barCode, int treshold,
+            int orderQuantity, double price) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.quantity = quantity;
+        this.barCode = barCode;
+        this.treshold = treshold;
+        this.orderQuantity = orderQuantity;
+        this.price = price;
+    }
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public int getBarCode() {
+        return barCode;
+    }
+
+    public void setBarCode(int barCode) {
+        this.barCode = barCode;
+    }
+
+    public int getTreshold() {
+        return treshold;
+    }
+
+    public void setTreshold(int treshold) {
+        this.treshold = treshold;
+    }
+
+    public int getOrderQuantity() {
+        return orderQuantity;
+    }
+
+    public void setOrderQuantity(int orderQuantity) {
+        this.orderQuantity = orderQuantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
     }
 
 }
