8de7142fe0099cbf7c6de1670d3e4c60faf6ddd3
chenyao <E0015290@u.nus.edu>
2016-03-27 17:43:32 +0800

    testcases of Member class and MemberManager class

--- src/sg/edu/nus/iss/se24_2ft/unit1/ca/customer/member/Member.java
+++ src/sg/edu/nus/iss/se24_2ft/unit1/ca/customer/member/Member.java
@@ -1,9 +1,9 @@
 package sg.edu.nus.iss.se24_2ft.unit1.ca.customer.member;
 
-import sg.edu.nus.iss.se24_2ft.unit1.ca.customer.Customer;
-
 import java.util.Arrays;
 import java.util.stream.Collectors;
+
+import sg.edu.nus.iss.se24_2ft.unit1.ca.customer.Customer;
 
 /**
  * @author: Tran Ngoc Hieu
@@ -13,7 +13,7 @@ public class Member implements Customer {
     private int loyaltyPoint, redeemPoint, addPoint;
 
     public Member(String requestedId, String name) {
-        //TODO: chk all other list methods, try validation
+        // TODO: chk all other list methods, try validation
         id = null;
 
         this.requestedId = requestedId;
@@ -24,6 +24,7 @@ public class Member implements Customer {
     public String getId() {
         return id;
     }
+
     public String getRequestedId() {
         return requestedId;
     }
@@ -38,55 +39,72 @@ public class Member implements Customer {
 
     @Override
     public String toString() {
-        return Arrays.asList(name, id, loyaltyPoint).stream()
-                .map(Object::toString)
-                .collect(Collectors.joining(","));
+        return Arrays.asList(name, id, loyaltyPoint).stream().map(Object::toString).collect(Collectors.joining(","));
     }
 
-    //setters
-    /*package*/ void setId () { id = requestedId; }
+    // setters
+    /* package */ void setId() {
+        id = requestedId;
+    }
 
-    /*package*/ void setLoyaltyPoint(int loyaltyPoint) {
+    /* package */ void setLoyaltyPoint(int loyaltyPoint) {
         this.loyaltyPoint = loyaltyPoint;
     }
 
-    //TODO: to be verified with CheckoutPanel
-//    public int setRedeemPoint(int point){
-//        if (loyaltyPoint < 0){
-//            redeemPoint = 0;
-//        } else if (point > loyaltyPoint) {
-//            //cannot redeem more than your current Loyalty Point
-//            redeemPoint = loyaltyPoint;
-//        } else {
-//            redeemPoint = point;
-//        }
-//        //Redeem base on multiple of 100 points
-//        redeemPoint = (redeemPoint/100) * 100;
-//        return redeemPoint;
-//    }
-//
-//    public float getRedeemPrice(){
-//        //Price redeem $5 for every 100 points
-//        float price = (redeemPoint/100)*5;
-//        return price;
-//    }
-//
-//    public int setAddPoint(float amountPaid){
-//        //dollar-to-point value: $10 spent to get 1 additional point
-//        addPoint = (int) (amountPaid / 10);
-//        return addPoint;
-//    }
-//
-//    public int updateLoyaltyPoint(){
-//        loyaltyPoint = loyaltyPoint - redeemPoint + addPoint;
-//        return loyaltyPoint;
-//    }
-//
-//    public int getRedeemPoint() {
-//        return redeemPoint;
-//    }
-//
-//    public int getAddPoint() {
-//        return addPoint;
-//    }
+    // TODO: to be verified with CheckoutPanel
+    // public int setRedeemPoint(int point){
+    // if (loyaltyPoint < 0){
+    // redeemPoint = 0;
+    // } else if (point > loyaltyPoint) {
+    // //cannot redeem more than your current Loyalty Point
+    // redeemPoint = loyaltyPoint;
+    // } else {
+    // redeemPoint = point;
+    // }
+    // //Redeem base on multiple of 100 points
+    // redeemPoint = (redeemPoint/100) * 100;
+    // return redeemPoint;
+    // }
+    //
+    // public float getRedeemPrice(){
+    // //Price redeem $5 for every 100 points
+    // float price = (redeemPoint/100)*5;
+    // return price;
+    // }
+    //
+    // public int setAddPoint(float amountPaid){
+    // //dollar-to-point value: $10 spent to get 1 additional point
+    // addPoint = (int) (amountPaid / 10);
+    // return addPoint;
+    // }
+    //
+    // public int updateLoyaltyPoint(){
+    // loyaltyPoint = loyaltyPoint - redeemPoint + addPoint;
+    // return loyaltyPoint;
+    // }
+    //
+    // public int getRedeemPoint() {
+    // return redeemPoint;
+    // }
+    //
+    // public int getAddPoint() {
+    // return addPoint;
+    // }
+
+    public boolean equals(Object member) {
+        if (member instanceof Member) {
+            Member m = (Member) member;
+            if (this.getRequestedId().equals(m.getRequestedId())) {
+                if (this.getName() == null)
+                    if (m.getName() == null)
+                        return true;
+                    else
+                        return false;
+                else if (m.getName() != null)
+                    if (this.getName().equals(m.getName()))
+                        return true;
+            }
+        }
+        return false;
+    }
 }
---
+++ test/sg/edu/nus/iss/se24_2ft/unit1/ca/MemberManagerTest.java
@@ -0,0 +1,106 @@
+package sg.edu.nus.iss.se24_2ft.unit1.ca;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import junit.framework.TestCase;
+import sg.edu.nus.iss.se24_2ft.unit1.ca.customer.member.Member;
+import sg.edu.nus.iss.se24_2ft.unit1.ca.customer.member.MemberManager;
+
+public class MemberManagerTest extends TestCase {
+    // test fixtures
+    private Member member1 = null;
+    private Member member2 = null;
+    private Member member3 = null;
+    private MemberManager memberManager = null;
+
+    @Before
+    public void setUp() throws Exception {
+        member1 = new Member("E0015280", "Gao Haijun");
+        member2 = new Member("E0015270", "Chen Yao");
+        member3 = new Member("R64565FG4", "Ang Lee");
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        member1 = null;
+        member2 = null;
+        memberManager = null;
+    }
+
+    // Test for MemberManger
+    @Test
+    public void testMemberManager() {
+        // non-existed file name
+        assertNull(memberManager);
+        try {
+            memberManager = new MemberManager("data/non-existed_file.dat");
+            fail("error path");
+        } catch (IOException e) {
+        }
+
+        if (null != memberManager)
+            memberManager = null;
+
+        // correct file name
+        assertNull(memberManager);
+        try {
+            memberManager = new MemberManager("data/Members.dat");
+        } catch (IOException e) {
+            e.printStackTrace();
+            fail("Exception thrown!");
+        }
+        assertTrue(memberManager != null);
+    }
+
+    @Test
+    public void testGetMember() {
+        assertNull(memberManager);
+        try {
+            memberManager = new MemberManager("data/Members.dat");
+        } catch (IOException e) {
+            e.printStackTrace();
+            fail("Exception thrown");
+        }
+        assertTrue(memberManager != null);
+        assertEquals(member3, memberManager.getMember("R64565FG4"));
+    }
+
+    @Test
+    public void testAddMember() {
+        assertNull(memberManager);
+        try {
+            memberManager = new MemberManager("data/Members.dat");
+            memberManager.addMember(member1);
+            memberManager.addMember(member2);
+        } catch (IOException e) {
+            e.printStackTrace();
+            fail("Exception thrown");
+        }
+        assertTrue(memberManager != null);
+        assertEquals(member1, memberManager.getMember("E0015280"));
+        assertEquals(member2, memberManager.getMember("E0015270"));
+    }
+
+    @Test
+    public void testGetMemberList() {
+        assertNull(memberManager);
+        try {
+            memberManager = new MemberManager("data/Members.dat");
+            memberManager.addMember(member1);
+            memberManager.addMember(member2);
+        } catch (IOException e) {
+            e.printStackTrace();
+            fail("Exception thrown");
+        }
+        List<Member> memberList = memberManager.getMemberList();
+        assertTrue(memberManager != null);
+        assertEquals(member3, memberList.get(2));
+        assertEquals(member1, memberList.get(3));
+        assertEquals(member2, memberList.get(4));
+    }
+}
