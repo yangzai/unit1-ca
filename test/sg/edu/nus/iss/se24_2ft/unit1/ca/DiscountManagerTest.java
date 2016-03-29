baeb76d65d354808fbe9c279ff1cf12a08ef2cc0
Nguyen Bui An Trung <nguyentrung0904@gmail.com>
2016-03-29 16:38:35 +0700

    Test get max discount for members and public customers

--- test/sg/edu/nus/iss/se24_2ft/unit1/ca/DiscountManagerTest.java
+++ test/sg/edu/nus/iss/se24_2ft/unit1/ca/DiscountManagerTest.java
@@ -6,6 +6,10 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import sg.edu.nus.iss.se24_2ft.unit1.ca.customer.Customer;
+import sg.edu.nus.iss.se24_2ft.unit1.ca.customer.PublicCustomer;
+import sg.edu.nus.iss.se24_2ft.unit1.ca.customer.member.Member;
+import sg.edu.nus.iss.se24_2ft.unit1.ca.customer.member.MemberManager;
 import sg.edu.nus.iss.se24_2ft.unit1.ca.discount.Discount;
 import sg.edu.nus.iss.se24_2ft.unit1.ca.discount.DiscountManager;
 
@@ -13,11 +17,12 @@ public class DiscountManagerTest {
 
 	private DiscountManager discountManager = null;
 	private Discount discount = null;
+	private Member member = null;
 
 	@Before
 	public void setUp() throws Exception {
 		discountManager = new DiscountManager("data/Discounts.dat");
-		discount = new Discount("TEST10", "TEST 2 Desc", null, -1, 30, true);
+		discount = new Discount("TEST11", "TEST 2 Desc", null, -1, 30, true);
 	}
 
 	@After
@@ -32,6 +37,12 @@ public class DiscountManagerTest {
 	}
 
 	@Test
+	public void testGetList() {
+		assertNotNull(discountManager.getDiscountList());
+		assertTrue(discountManager.getDiscountList().size() > 0);	
+	}
+
+	@Test
 	public void testAddDiscount() {
 		int lastSize = discountManager.getDiscountList().size();
 		assertNotNull(discount);
@@ -40,8 +51,22 @@ public class DiscountManagerTest {
 		Discount addedDiscount = discountManager.getDiscountList().get(lastSize);
 		assertTrue(addedDiscount.getCode().equals(discount.getRequestedCode()));
 		assertTrue(discountManager.addDiscount(discount) == false);
-
-		
 	}
 
+	@Test
+	public void testGetDiscount(){
+		Customer customer = new Member("TESTMEMBERID1", "NGUYEN TRUNG");
+		Customer publicCustomer = PublicCustomer.getInstance();
+		Discount d = discountManager.getMaxDiscount(publicCustomer);
+		assertTrue(!d.isMemberOnly());
+		assertTrue(d.isDiscountAvailable());
+
+		System.out.println(d.toString());
+		d = discountManager.getMaxDiscount(customer);
+		assertTrue(d.getCode().equals("MEMBER_FIRST"));
+		assertTrue(d.isMemberOnly());
+		assertTrue(d.isDiscountAvailable());
+
+		System.out.println(d.toString());
+	}
 }
