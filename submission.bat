40071ff0596b8b75de341397b4edb4b85825419a
navygao <370254726@qq.com>
2016-03-31 17:14:01 +0800

    Re-commit ---- Add submission.bat which will re-arrange folder and compress it automatically, as a part of deployment

--- submission.bat
+++ submission.bat
@@ -1,90 +1,50 @@
 @ECHO OFF
-REM Pre-Condition: None
-REM ********************************
-REM Created by Navy Gao on 29/3/2016
-REM ********************************
-REM %1 = Compile switch, compile all and compile mini
+SETLOCAL
 
-REM *********************************
-REM Get variables
-REM *********************************
 IF "%~1"=="" (
-	SET CompileOpiton=CompileMini
+	SET zipfile=se24_F02.zip
 ) ELSE (
-	SET CompileOpiton=%1
+	SET zipfile=%~1
 )
 
-SETLOCAL ENABLEDELAYEDEXPANSION
-REM //Setup enviorment
+REM Cleanup directories
+IF EXIST "%cd%\submission\classes" RMDIR /Q /S "%cd%\submission\classes" 
+IF EXIST "%cd%\submission\src" RMDIR /Q /S "%cd%\submission\src" 
+IF EXIST "%cd%\submission\test" RMDIR /Q /S "%cd%\submission\test" 
+IF EXIST "%cd%\submission\data" RMDIR /Q /S "%cd%\submission\data" 
 
-IF EXIST setenv.bat (
-	CALL setenv.bat
-) ELSE (
-	REG QUERY "HKEY_LOCAL_MACHINE\SOFTWARE\JavaSoft\Java Development Kit\1.8" /v JavaHome | findstr "JavaHome">>temp.dat
-	FOR /F "Tokens=1,2,*" %%s IN (temp.dat) DO (
-		SET JAVA_HOME=%%u
-		SET PATH=!JAVA_HOME!\bin;!PATH!
-	)
-	DEL temp.dat /Q 1>NUL 2>&1
-)
+REM Populate directories
+XCOPY "%cd%\src" "%cd%\submission\src" /S /R /I
+XCOPY "%cd%\test" "%cd%\submission\test" /S /R /I
+XCOPY "%cd%\data-sample" "%cd%\submission\data" /S /R /I
 
-SET /A pstatus=0
-SET ClassDir=%cd%\classes
-SET SourcePath_Prefix=src\sg\edu\nus\iss\se24_2ft\unit1\ca
-SET FullSourcePath_Prefix=%cd%\src\sg\edu\nus\iss\se24_2ft\unit1\ca
-SET CLASSPATH=.;%JAVA_HOME%\lib;%JAVA_HOME%\lib\tools.jar;%cd%\classses;%cd%\src;%CLASSPATH%;
+REM zip
+rem CD /D %~dp0
+CALL :ZipFile "%cd%\submission\" "%cd%\%zipfile%" || SET /A pstatus=1
 
-REM //Create classes path
-IF EXIST "%ClassDir%" RMDIR /Q /S "%ClassDir%" 1>NUL 2>&1
+EXIT /B %pstatus%
 
-MKDIR "%ClassDir%"
+:ZipFile <Source folder> <newzipfile>
+SET vbsfile="temp.vbs"
+IF EXIST %vbs% DEL /F /Q %vbsfile%
 
-REM //Compiling
-Echo Compiling... & Echo Compiling...>%~n0.log
-IF "%CompileOpiton%"=="CompileMini" (
-	CALL :CompileMini || SET /A pstatus=1
-) ELSE (
-	CALL :CompileAll || SET /A pstatus=2
-)
+ECHO CreateObject("Scripting.FileSystemObject").CreateTextFile(%2, True).Write "PK" ^& Chr(5) ^& Chr(6) ^& String(18, vbNullChar)>%vbsfile%
+ECHO Set objShell = CreateObject("Shell.Application")>>%vbsfile%
+ECHO Set source = objShell.NameSpace(%1).Items>>%vbsfile%
+ECHO objShell.NameSpace(%2).CopyHere(source)>>%vbsfile%
+ECHO wScript.Sleep 2000>>%vbsfile%
 
-GOTO :END
-
-REM ****************** Functions **************************
-
-:::::::::::::::::::::::CompileAll  :::::::::::::::::::::::::::
-REM //Compile all java files under specified folder
-
-:CompileAll
-FOR /R "%FullSourcePath_Prefix%" %%s in (.) do (
-	ECHO %%s
-	javac -d "%cd%\classes" -cp classes -sourcepath src "%%s"\*.java>>%~n0.log
-	IF !ERRORLEVEL! NEQ 0 SET /A status=1
-) 
-
-IF DEFINED status IF !status!==1 (
-	EXIT /B 1
-)
-GOTO :EOF
-
-:::::::::::::::::::::::CompileMini  :::::::::::::::::::::::::::
-REM //Compile only related files which application running needs
-:CompileMini
-
-javac -d "%cd%\classes" -cp classes -sourcepath src %SourcePath_Prefix%\*.java 1>>%~n0.log 2>>&1
+echo.
+echo Compressing files...
+cscript //nologo %vbsfile%
 IF %ERRORLEVEL% EQU 0 (
+	if exist %vbsfile% del /f /q %vbsfile%
+	echo Compression Succeed^!
 	EXIT /B 0
 ) ELSE (
+	if exist %vbsfile% del /f /q %vbsfile%
+	echo Compression error^!
 	EXIT /B 1
 )
-GOTO :EOF
 
-:END
-IF %pstatus% GTR 0 (
-	ECHO Compile error^! & Echo Compile error^!>>%~n0.log
-	IF EXIST "%ClassDir%" RMDIR /Q /S "%ClassDir%" 1>NUL 2>&1
-) ELSE (
-	ECHO Compile Succeed^! & Echo Compile Succeed^!>>%~n0.log
-)
-EXIT /B %pstatus%
-ENDLOCAL
-@ECHO ON
+GOTO :EOF
