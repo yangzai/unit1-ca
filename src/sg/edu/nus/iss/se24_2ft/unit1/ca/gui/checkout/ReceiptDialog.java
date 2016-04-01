7dacf5b56b16890939d427d6b5d47ac0e0d282e3
tran0040 <hieu2608@gmail.com>
2016-04-01 15:51:13 +0800

    Fixes #13 : Add Receipt Printer to console

--- src/sg/edu/nus/iss/se24_2ft/unit1/ca/gui/checkout/ReceiptDialog.java
+++ src/sg/edu/nus/iss/se24_2ft/unit1/ca/gui/checkout/ReceiptDialog.java
@@ -8,13 +8,17 @@ import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import javax.swing.*;
 
 import sg.edu.nus.iss.se24_2ft.unit1.ca.customer.Customer;
 import sg.edu.nus.iss.se24_2ft.unit1.ca.customer.member.Member;
 import sg.edu.nus.iss.se24_2ft.unit1.ca.discount.Discount;
+import sg.edu.nus.iss.se24_2ft.unit1.ca.printer.ReceiptPrinter;
+import sg.edu.nus.iss.se24_2ft.unit1.ca.product.Product;
 import sg.edu.nus.iss.se24_2ft.unit1.ca.transaction.Transaction;
+import sg.edu.nus.iss.se24_2ft.unit1.ca.transaction.TransactionItem;
 import sg.edu.nus.iss.se24_2ft.unit1.ca.transaction.TransactionManager;
 import sg.edu.nus.iss.se24_2ft.unit1.ca.util.Util;
 
@@ -24,7 +28,9 @@ import sg.edu.nus.iss.se24_2ft.unit1.ca.util.Util;
  *
  */
 /*package*/ class ReceiptDialog extends JDialog {
-    private Transaction transaction;
+
+
+	private Transaction transaction;
     private Discount discount;
     private Member member;
     private JTable table;
@@ -98,7 +104,7 @@ import sg.edu.nus.iss.se24_2ft.unit1.ca.util.Util;
         gbc.weightx = 0.2;
         gbc.anchor = GridBagConstraints.EAST;
         double discountPercent = discount != null ? discount.getPercent() : 0;
-        infoPanel.add(new JLabel("Discount: (" + discountPercent + "%)"), gbc);
+        infoPanel.add(new JLabel("Discount (" + discountPercent + "%):"), gbc);
 
         gbc.gridx++;
         gbc.weightx = 0.5;
@@ -199,4 +205,59 @@ import sg.edu.nus.iss.se24_2ft.unit1.ca.util.Util;
 
         return p;
     }
+    
+    @Override
+	public void setVisible(boolean arg0) {
+    	if (arg0) ReceiptPrinter.print(this.toString());
+		super.setVisible(arg0);
+	}
+    
+    public String toString(){
+    	String strTableFormat = "%-6.6s  %-7.7s %-30.30s  %-10.10s %n";
+    	String strTotalFormat = "%-30.30s %-10.10s %n";
+    	
+    	//Init data for Receipt:
+    	Integer id = transaction != null ? transaction.getId() : null;
+    	Date date = transaction != null ? transaction.getDate() : null;
+    	double subtotal = transaction != null ? transaction.getSubtotal() : 0;
+    	double discountPercent = discount != null ? discount.getPercent() : 0;
+    	String discountLabel = "Discount (" + discountPercent + "%):";
+    	double discountAmount = transaction != null ? transaction.getDiscountAmount() : 0;
+    	double redeemValue = transaction != null ? transaction.getRedeemPointValue() : 0;
+    	double payment = transaction != null ? transaction.getPayment() : 0;
+    	double change = transaction != null ? transaction.getBalance() * -1 : 0;
+    	String redeemPointLabel = "Redeemed (1 point = $" + TransactionManager.DOLLAR_TO_POINT + "):";
+    	int redeemPoint = transaction != null ? transaction.getRedeemPoint() : 0;
+    	String creditPointLabel = "Credited ($10 = " + TransactionManager.POINT_TO_DOLLAR + "point):";
+    	int creditPoint = transaction != null ? transaction.getCreditPoint() : 0;
+    	int balancePoint = member != null ? member.getLoyaltyPoint() : 0;
+    	
+    	//Formatted Text
+    	String string = "********* Transaction ID: " + id + " **************" + "\n";
+    	string += "Issue Date: " + (new SimpleDateFormat("MM/dd/yyyy HH:mm:ss")).format(date) + "\n\n";
+    	string += String.format(strTableFormat, "Qty", "ID", "Description", "Unit Price");
+    	for (TransactionItem item : transaction.getTransactionItemList()) {
+    		Product product = item.getProduct();
+			string += String.format(strTableFormat, item.getQuantity(), product.getId(), product.getDescription(), "$"+product.getPrice());
+		}
+    	string += "\n";
+    	string += String.format(strTotalFormat, "Subtotal: ", Util.formatDollar(subtotal));
+    	string += String.format(strTotalFormat, discountLabel, Util.formatDollar(discountAmount));
+    	if (member!=null) {
+    		string += String.format(strTotalFormat, "Redeem Value:", Util.formatDollar(discountAmount));
+		}
+    	string += "\n";
+    	string += String.format(strTotalFormat, "Cash:", Util.formatDollar(payment));
+    	string += "\n";
+    	string += String.format(strTotalFormat, "Change:", Util.formatDollar(change));
+    	string += "\n";
+    	if (member!=null) {
+    		string += String.format(strTotalFormat, redeemPointLabel, redeemPoint);
+    		string += String.format(strTotalFormat, creditPointLabel, creditPoint);
+    		string += String.format(strTotalFormat, "Point Balance:", balancePoint);
+		}
+    	return string;
+    }
+    
+    
 }
